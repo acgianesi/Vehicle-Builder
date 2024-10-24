5fff269c976e5240e4b744631e0bb25c43aec56c
André Company Gianesi <andrecompanygianesi@Andres-MacBook-Air.local>
2024-10-24 16:05:52 -0400

    override the printDetails method from vehicle class

--- Develop/src/classes/Motorbike.ts
+++ Develop/src/classes/Motorbike.ts
@@ -50,6 +50,7 @@ wheelie(){
   console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`);
 }
   // TODO: Override the printDetails method from the Vehicle class
+  override printDetails(): void {    
   // TODO: The method should call the printDetails method of the parent class
   // TODO: The method should log the details of the Motorbike
   // TODO: The details should include the VIN, make, model, year, weight, top speed, color, and wheels
