4e1d1a37112cfa2c4ee161a9b09ccc3048f99ebe
André Company Gianesi <andrecompanygianesi@Andres-Air.lan>
2024-10-25 14:54:05 -0400

    added option to tow and wheelie

--- Develop/src/classes/Cli.ts
+++ Develop/src/classes/Cli.ts
@@ -335,6 +335,8 @@ class Cli {
             'Turn right',
             'Turn left',
             'Reverse',
+            'Tow a vehicle',
+            'Wheelie',
             'Select or create another vehicle',
             'Exit',
           ],
